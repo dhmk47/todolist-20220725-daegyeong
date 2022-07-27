469895dd26bd91a290a75bfa562d540b03d461dd
dhmk47 <dhmk47@naver.com>
2022-07-27 21:52:04 +0900

    todo

--- src/main/java/com/daegyeong/todolist/domain/todo/TodoRepository.java
+++ src/main/java/com/daegyeong/todolist/domain/todo/TodoRepository.java
@@ -9,4 +9,9 @@ import org.apache.ibatis.annotations.Mapper;
 public interface TodoRepository {
 	public int save(Todo Todo) throws Exception;
 	public List<Todo> getTodoListOfIndex(Map<String, Object> map) throws Exception;
+	public List<Todo> getImportanceTodoListOfIndex(Map<String, Object> map) throws Exception;
+	public int updateTodoComplete(int todo_code) throws Exception;
+	public int updateTodoImportance(int todo_code) throws Exception;
+	public int updateTodoByTodoCode(Todo todo) throws Exception;
+	public int remove(int todo_codoe) throws Exception;
 }
--- src/main/java/com/daegyeong/todolist/service/todo/TodoServiceImpl.java
+++ src/main/java/com/daegyeong/todolist/service/todo/TodoServiceImpl.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
@@ -11,6 +12,7 @@ import com.daegyeong.todolist.domain.todo.Todo;
 import com.daegyeong.todolist.domain.todo.TodoRepository;
 import com.daegyeong.todolist.web.dto.todo.CreateTodoReqDto;
 import com.daegyeong.todolist.web.dto.todo.TodoListRespDto;
+import com.daegyeong.todolist.web.dto.todo.UpdateTodoReqDto;
 
 import lombok.RequiredArgsConstructor;
 
@@ -24,28 +26,48 @@ public class TodoServiceImpl implements TodoService{
 	public boolean createTodo(CreateTodoReqDto createTodoReqDto) throws Exception {
 		Todo todoEntity = createTodoReqDto.toEntity();
 
-		String content = todoEntity.getTodo_content();
-		for(int i = 200; i < 1000; i++) {
-			todoEntity.setTodo_content(content + "_" + (i + 1));
-			if(i % 2 == 0) {
-				todoEntity.setImportance_flag(1);
-			}else {
-				todoEntity.setImportance_flag(0);
-			}
-			todoRepository.save(todoEntity);
-		}
-		return true;
+//		String content = todoEntity.getTodo_content();
+//		for(int i = 200; i < 1000; i++) {
+//			todoEntity.setTodo_content(content + "_" + (i + 1));
+//			if(i % 2 == 0) {
+//				todoEntity.setImportance_flag(1);
+//			}else {
+//				todoEntity.setImportance_flag(0);
+//			}
+//			todoRepository.save(todoEntity);
+//		}
+//		return true;
 		
-//		return todoRepository.save(todoEntity) > 0;
+		return todoRepository.save(todoEntity) > 0;
 	}
 
 	@Override
 	public List<TodoListRespDto> getTodoList(int page, int contentCount) throws Exception {
+		
+		List<Todo> todoList = todoRepository.getTodoListOfIndex(createGetTdoListMap(page, contentCount));
+		
+		return createTodoListRespDtos(todoList);
+	}
+	
+	@Override
+	public List<TodoListRespDto> getImportanceTodoList(int page, int contentCount) throws Exception {
+		
+		List<Todo> todoList = todoRepository.getImportanceTodoListOfIndex(createGetTdoListMap(page, contentCount));
+		
+		
+		return createTodoListRespDtos(todoList);
+	}
+	
+	private Map<String, Object> createGetTdoListMap(int page, int contentCount) {
 		Map<String, Object> map = new HashMap<String, Object>();
 		map.put("index", (page - 1) * contentCount);
 		map.put("count", contentCount);
 		
-		List<Todo> todoList = todoRepository.getTodoListOfIndex(map);
+		return map;
+	}
+
+	private List<TodoListRespDto> createTodoListRespDtos(List<Todo> todoList) {
+		
 		List<TodoListRespDto> todoListRespDtos = new ArrayList<TodoListRespDto>();
 		
 		todoList.forEach(todo -> {
@@ -55,4 +77,23 @@ public class TodoServiceImpl implements TodoService{
 		return todoListRespDtos;
 	}
 
+	@Override
+	public boolean updateTodoComplete(int todoCode) throws Exception {
+		return todoRepository.updateTodoComplete(todoCode) > 0;
+	}
+
+	@Override
+	public boolean updateTodoImportance(int todoCode) throws Exception {
+		return todoRepository.updateTodoImportance(todoCode) > 0;
+	}
+
+	@Override
+	public boolean updateTodo(UpdateTodoReqDto updateTodoReqDto) throws Exception {
+		return todoRepository.updateTodoByTodoCode(updateTodoReqDto.toEntity()) > 0;
+	}
+	
+	@Override
+	public boolean removeTodo(int todoCode) throws Exception {
+		return todoRepository.remove(todoCode) > 0;
+	}
 }
